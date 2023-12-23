1836ddaeaaff57db0f4bd2b3fa9193de5960e222
razuahmeds <rukunuzzaman.razu@gmail.com>
2023-12-24 01:05:13 +0600

    connit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,13 @@ import AboutMe from "../components/AboutMe";
 import Banner from "../components/Banner";
 import Blogs from "../components/Blogs";
 import Contact from "../components/Contact";
+import Project from "../components/Project";
 
 function Home() {
   return (
     <div>
       <Banner />
+      <Project />
       <AboutMe />
       <Blogs />
       <Contact />
