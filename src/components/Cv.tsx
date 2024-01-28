7669a7d6c1f77a086049a78083a25df28ac701e9
razuahmeds <rukunuzzaman.razu@gmail.com>
2024-01-28 19:52:41 +0600

    cv add

--- src/components/Cv.tsx
+++ src/components/Cv.tsx
@@ -3,7 +3,11 @@ import "../styles/cv.css";
 function Cv() {
   return (
     <div>
-      <img className="cv" src="https://i.ibb.co/7QBJqWw/CV.jpg" alt="" />
+      <img
+        className="cv"
+        src="https://i.ibb.co/rHWM04J/Razu-CV-Resume.jpg"
+        alt=""
+      />
     </div>
   );
 }
